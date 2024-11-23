2f7cfb86ec707d0b9d82572ca75661627fd35479
René Abreu <rene@reneabreu.dev>
2024-11-23 15:57:31 -0300

    updated editor title modifier

--- Editor/EditorWindow/EditorWindowTitleModifier.cs
+++ Editor/EditorWindow/EditorWindowTitleModifier.cs
@@ -3,42 +3,79 @@
  */
 
 using UnityEditor;
+using UnityEngine;
 
 namespace UltimateLazy.Tools.Editor
 {
     [InitializeOnLoad]
-    public class EditorWindowTitleModifier
+    public class EditorWindowTitleModifier : IUltimateLazyToolWindowTab
     {
         private static bool _autoUpdate = true;
 
-        private static string _defaultTitle = "{activeScene} - {applicationTitle} - {platform} - {unityVersion} - {gitBranch}";
+        private const string _defaultTitle =
+            "{activeScene} - {applicationTitle} - {platform} - {unityVersion} - {gitBranch}";
+        private static string _title = string.Empty;
         private const string kEditorPrefsKey = "tuld_WindowTitle";
-        
+
+        public string WindowName => "Editor Window Title Modifier";
+        public string TabName => "Editor Window Title Modifier";
+
         static EditorWindowTitleModifier()
         {
+            _title = EditorPrefs.GetString(kEditorPrefsKey, _defaultTitle);
+
             if (_autoUpdate)
             {
                 EditorApplication.update += UpdateWindowTitle;
             }
         }
-        
-        [MenuItem("Tools/The Ultimate Lazy Dev/Editor/Update Main Window Title", priority = 2)]
+
+        public void OnGUI()
+        {
+            _title = EditorGUILayout.TextField($"Window Title", _title);
+
+            if (GUILayout.Button("Reset to Default"))
+            {
+                _title = _defaultTitle;
+                GUI.FocusControl(null);
+                //Repaint();
+                SaveTitle();
+                UpdateWindowTitle();
+            }
+
+            if (GUILayout.Button("Update Window Title"))
+            {
+                GUI.FocusControl(null);
+                //Repaint();
+                SaveTitle();
+                UpdateWindowTitle();
+            }
+        }
+
+        private static void SaveTitle()
+        {
+            EditorPrefs.SetString(kEditorPrefsKey, _title);
+        }
+
+        //[MenuItem("Tools/The Ultimate Lazy Dev/Editor/Update Main Window Title", priority = 2)]
         public static void UpdateWindowTitle()
         {
             EditorApplication.updateMainWindowTitle += UpdateMainWindowTitle;
             EditorApplication.UpdateMainWindowTitle();
             EditorApplication.updateMainWindowTitle -= UpdateMainWindowTitle;
         }
-        
-        private static void UpdateMainWindowTitle(ApplicationTitleDescriptor applicationTitleDescriptor)
+
+        private static void UpdateMainWindowTitle(
+            ApplicationTitleDescriptor applicationTitleDescriptor
+        )
         {
-            var title = EditorPrefs.GetString(kEditorPrefsKey,_defaultTitle);
-            applicationTitleDescriptor.title = ParseTitle(applicationTitleDescriptor, title);
+            applicationTitleDescriptor.title = ParseTitle(applicationTitleDescriptor, _title);
         }
 
         private static string ParseTitle(ApplicationTitleDescriptor titleDescriptor, string title)
         {
-            return title.Replace("{activeScene}", titleDescriptor.activeSceneName)
+            return title
+                .Replace("{activeScene}", titleDescriptor.activeSceneName)
                 .Replace("{applicationTitle}", titleDescriptor.projectName)
                 .Replace("{platform}", titleDescriptor.targetName)
                 .Replace("{unityVersion}", titleDescriptor.unityVersion)
